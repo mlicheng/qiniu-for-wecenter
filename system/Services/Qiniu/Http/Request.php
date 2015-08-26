33fbfa16d55ba9d91da09acb2e2fe56db5b7a575
mlicheng <mlicheng>
2015-08-26 13:41:52 +0800

    Qiniu for wecenter
    Qiniu for wecenter

---
+++ system/Services/Qiniu/Http/Request.php
@@ -0,0 +1,17 @@
+<?php
+
+class Services_Qiniu_Http_Request
+{
+    public $url;
+    public $headers;
+    public $body;
+    public $method;
+
+    public function __construct($method, $url, array $headers = array(), $body = null)
+    {
+        $this->method = strtoupper($method);
+        $this->url = $url;
+        $this->headers = $headers;
+        $this->body = $body;
+    }
+}
